d66dfb545ef1109d2552e44efdcd15162cc94910
limpit666 <omar.farahc@hotmail.com>
2020-08-14 17:22:29 +0100

    Initial commit.

---
+++ Source/Argo/ArgoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArgoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARGO_API AArgoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
